c254175ed3b2bbe0d12b8507bc6914e82ea7ab3d
Cesar Pires <cesar.augusto.pires@hotmail.com>
2023-06-29 10:38:53 -0300

    feat(spring-boot): add Spring Boot skeleton
    Adds the basic structure and configuration files required for a Spring Boot project. Includes the main application class, application properties, and initial folder structure.

---
+++ src/main/java/com/courses/kafkaticketsales/KafkaTicketSalesApplication.java
@@ -0,0 +1,13 @@
+package com.courses.kafkaticketsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTicketSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaTicketSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/courses/kafkaticketsales/KafkaTicketSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courses.kafkaticketsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTicketSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
