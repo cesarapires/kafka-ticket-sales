0a6165c496eec28b663185143329a6310736273a
Cesar Pires <cesar.augusto.pires@hotmail.com>
2023-08-11 11:15:13 -0300

    refactor: remove port and add interface like port to TicketSalesService

---
+++ src/main/java/com/courses/kafkaticketsales/application/ports/input/TicketSalesInterface.java
@@ -0,0 +1,18 @@
+package com.courses.kafkaticketsales.application.ports.input;
+
+import com.courses.kafkaticketsales.domain.model.ticketSales.TicketSales;
+
+import java.util.UUID;
+import java.util.List;
+
+
+public interface TicketSalesInterface {
+
+    void addTicketSales(TicketSales ticketSales);
+
+    TicketSales getTicketSaleById(UUID uuid);
+
+    List<TicketSales> getTicketSaleByUser(UUID uuidUser);
+
+    void updateTicketStatus(UUID uuid, String status);
+};
