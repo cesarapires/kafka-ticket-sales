b6df354479ca57dd4a4b4a0eafdaffbc333017aa
Cesar Pires <cesar.augusto.pires@hotmail.com>
2023-08-18 20:15:44 -0300

    feat(infrastructure): add TicketSalesRestAdapter

---
+++ src/main/java/com/courses/kafkaticketsales/infrastructure/adapters/input/rest/TickerSalesRestAdapter.java
@@ -0,0 +1,34 @@
+package com.courses.kafkaticketsales.infrastructure.adapters.input.rest;
+
+import com.courses.kafkaticketsales.domain.model.ticketSales.TicketSales;
+import com.courses.kafkaticketsales.application.ports.input.TicketSalesServiceInterface ;
+import com.courses.kafkaticketsales.infrastructure.adapters.input.rest.data.request.TicketSalesCreateRequest;
+import com.courses.kafkaticketsales.infrastructure.adapters.input.rest.mapper.TicketSalesRestMapper;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+@RequiredArgsConstructor
+public class TickerSalesRestAdapter {
+
+    private final TicketSalesServiceInterface ticketSalesService;
+
+    private final TicketSalesRestMapper ticketSalesRestMapper;
+
+    @PostMapping(value = "/ticket-sales")
+    public ResponseEntity<TicketSales> createTicketSales(@RequestBody @Valid TicketSalesCreateRequest ticketSalesCreateRequest) {
+
+        TicketSales ticketSales = ticketSalesRestMapper.toTicketSales(ticketSalesCreateRequest);
+
+        ticketSales = ticketSalesService.addTicketSales(ticketSales);
+
+        return new ResponseEntity<>(ticketSales, HttpStatus.CREATED);
+    }
+}
