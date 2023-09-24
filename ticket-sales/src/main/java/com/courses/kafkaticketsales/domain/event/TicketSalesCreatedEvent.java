1c357f8742095c9ddc6ffeb02d9da7efb689610e
Cesar Pires <cesar.augusto.pires@hotmail.com>
2023-09-23 21:38:52 -0300

    chore: remove annotations that are not being used

--- ticket-sales/src/main/java/com/courses/kafkaticketsales/domain/event/TicketSalesCreatedEvent.java
+++ ticket-sales/src/main/java/com/courses/kafkaticketsales/domain/event/TicketSalesCreatedEvent.java
@@ -6,11 +6,7 @@ import lombok.*;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Builder
 @Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class TicketSalesCreatedEvent implements TicketSalesEvent {
 
     private UUID id;
--- ticket-sales/src/main/java/com/courses/kafkaticketsales/infrastructure/adapters/input/consumer/event/TicketSalesWasReceived.java
+++ ticket-sales/src/main/java/com/courses/kafkaticketsales/infrastructure/adapters/input/consumer/event/TicketSalesWasReceived.java
@@ -6,19 +6,10 @@ import lombok.*;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Builder
 @Getter
-@Setter
-@AllArgsConstructor
 public class TicketSalesWasReceived implements TicketSalesEvent {
-    private UUID id;
 
     private LocalDateTime dateTime;
-
-    public TicketSalesWasReceived(UUID id) {
-        this.id = id;
-        this.dateTime = LocalDateTime.now();
-    }
 
     public TicketSalesWasReceived() {
         this.dateTime = LocalDateTime.now();
