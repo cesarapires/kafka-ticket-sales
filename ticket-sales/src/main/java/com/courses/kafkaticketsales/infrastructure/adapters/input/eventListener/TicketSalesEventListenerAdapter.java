4630e3529b3323bc34e3a223407fff6b2e9282ca
Cesar Pires <cesar.augusto.pires@hotmail.com>
2023-09-23 21:40:08 -0300

    chore: remove annotation from slf4j and add log as listener attribute

--- ticket-sales/src/main/java/com/courses/kafkaticketsales/infrastructure/adapters/input/eventListener/TicketSalesEventListenerAdapter.java
+++ ticket-sales/src/main/java/com/courses/kafkaticketsales/infrastructure/adapters/input/eventListener/TicketSalesEventListenerAdapter.java
@@ -1,16 +1,18 @@
 package com.courses.kafkaticketsales.infrastructure.adapters.input.eventListener;
 
 import com.courses.kafkaticketsales.domain.event.TicketSalesCreatedEvent;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
-@Slf4j
 public class TicketSalesEventListenerAdapter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TicketSalesEventListenerAdapter.class);
 
     @EventListener
     public void handle(TicketSalesCreatedEvent event) {
-        log.info("Ticket Sales created with id " + event.getId() + " at " + event.getDateTime());
+        LOGGER.info("Ticket Sales created with id " + event.getId() + " at " + event.getDateTime());
     }
 }
--- ticket-sales/src/main/java/com/courses/kafkaticketsales/infrastructure/adapters/input/eventListener/TicketSalesKafkaEventListenerAdapter.java
+++ ticket-sales/src/main/java/com/courses/kafkaticketsales/infrastructure/adapters/input/eventListener/TicketSalesKafkaEventListenerAdapter.java
@@ -1,17 +1,18 @@
 package com.courses.kafkaticketsales.infrastructure.adapters.input.eventListener;
 
-import com.courses.kafkaticketsales.domain.event.TicketSalesCreatedEvent;
 import com.courses.kafkaticketsales.infrastructure.adapters.input.consumer.event.TicketSalesWasReceived;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 public class TicketSalesKafkaEventListenerAdapter {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TicketSalesKafkaEventListenerAdapter.class);
+
     @EventListener
     public void handle(TicketSalesWasReceived event) {
-        log.info("Ticket Sales was received at " + event.getDateTime());
+        LOGGER.info("Ticket Sales was received at " + event.getDateTime());
     }
 }
